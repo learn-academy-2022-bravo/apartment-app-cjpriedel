cba75047e4deac1d6862033752dc2fc56f208fb2
cjpriedel <cjpriedel@gmail.com>
2022-05-17 15:54:20 -0700

    basic info added to Home page

--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -3,7 +3,11 @@ import React, { Component } from 'react'
 export default class Home extends Component {
   render() {
     return (
-      <h3>Home</h3>
+      <>
+      <h1>Welcome to Apartment City</h1>
+      <h3>Where its not just an apartment, it's also in a city.</h3>
+      <p> Sign up or login to view all of the wonderful luxury apartments we offer...in a city.</p>
+      </>
     )
   }
 }
